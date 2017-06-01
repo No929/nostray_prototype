109f4ddd442fde06f9358d6f090fb4c46731be3e
Klawens <lscllyx@icloud.com>
2017-06-01 22:08:47 +0800

    Comments table

--- community/models.py
+++ community/models.py
@@ -13,10 +13,9 @@ class Posts(models.Model):
 		('TS', 'tell_story'),
 	]
 	ANIMAL_CATE_CHOICES = [
-		(),
-		(),
-		(),
-		(),
+		('DOG', 'dog'),
+		('CAT', 'cat'),
+		('OTHER', 'other'),
 	]
 	post_id = models.AutoField(max_length=10, primary_key=True, verbose_name=u"帖子主键ID")
 	title = models.CharField(max_length=20, verbose_name=u"帖子标题")
@@ -25,11 +24,30 @@ class Posts(models.Model):
 	mod_time = models.DateTimeField(auto_now_add=True, verbose_name=u"修订时间")
 	image = models.ImageField(upload_to='post_img', null=True, verbose_name=u"贴图")
 	user_id = models.IntegerField(verbose_name=u"所属用户")
-	post_cate = models.CharField(max_length=2, verbose_name="帖子类型")
-	animal_cate = models.CharField(max_length=10, null=True, verbose_name="动物类型")
+	post_cate = models.CharField(max_length=2, choices=POST_CATE_CHOICES, verbose_name="帖子类型")
+	animal_cate = models.CharField(max_length=10, choices=ANIMAL_CATE_CHOICES, null=True, verbose_name="动物类型")
+	comments = models.IntegerField(default=0, verbose_name=u"评论数量")
+	likes = models.IntegerField(default=0, verbose_name=u"顶")
+	dislikes = models.IntegerField(default=0, verbose_name=u"踩")
 
 	class Meta:
 		verbose_name = u"帖子"
 		verbose_name_plural = verbose_name
-		db_table = "post"
-		ordering = "-mod_time"
+		db_table = "posts"
+		ordering = ['-mod_time']
+
+
+class Comments(models.Model):
+	comment_id = models.AutoField(max_length=10, primary_key=True, verbose_name=u"评论id")
+	content = models.CharField(max_length=256, verbose_name=u"内容")
+	user_id = models.IntegerField(verbose_name=u"所属用户")
+	image = models.ImageField(upload_to='comment_img', null=True, verbose_name=u"评论图片")
+	add_time = models.DateTimeField(default=timezone.now, verbose_name=u"评论时间")
+	likes = models.IntegerField(default=0, verbose_name=u"顶")
+	dislikes = models.IntegerField(default=0, verbose_name=u"踩")
+
+	class Meta:
+		verbose_name = u"评论"
+		verbose_name_plural = verbose_name
+		db_table = "comments"
+		ordering = ['-add_time']
--- register/models.py
+++ register/models.py
@@ -17,6 +17,7 @@ class UserInfo(models.Model):
 	user_id = models.AutoField(max_length=10, primary_key=True, verbose_name=u"用户主键ID")
 	username = models.CharField(max_length=16, null=True, blank=True, verbose_name=u"用户名")
 	password = models.CharField(max_length=16,verbose_name=u"密码")
+	introduce = models.CharField(max_length=100, verbose_name=u"介绍")
 	kind = models.CharField(max_length=1, choices=USER_KIND_CHOICES, verbose_name=u"用户类型")
 	mod_time = models.DateTimeField(auto_now_add=True, verbose_name="修订时间")
 	add_time = models.DateTimeField(default=timezone.now, verbose_name=u"注册时间")
@@ -26,10 +27,12 @@ class UserInfo(models.Model):
 	icon = models.ImageField(upload_to='user_icon', null=True, verbose_name=u"头像")
 	photo = models.ImageField(upload_to='user_photo', null=True, verbose_name=u"相册")
 	creditrating = models.IntegerField(default=60, verbose_name=u"信誉评分")
+	likes = models.IntegerField(default=0, verbose_name=u"顶")
+	dislikes = models.IntegerField(default=0, verbose_name=u"踩")
 	#ip = models.IPAdressField(null=True, blank=True, verbose_name=u"IP地址")
 
 	class Meta:
 		verbose_name = u"用户信息"
 		verbose_name_plural = verbose_name
-		db_table = "user"
-		ordering = "-user_id"
+		db_table = "userinfo"
+		ordering = ['-user_id']
