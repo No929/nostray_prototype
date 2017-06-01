0d5aff61cea0bfd95789615573c377095cc0ee87
Klawens <lscllyx@icloud.com>
2017-06-01 20:38:41 +0800

    Models change

--- community/models.py
+++ community/models.py
@@ -2,5 +2,34 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+import django.utils.timezone as timezone
 # Create your models here.
+
+class Posts(models.Model):
+	POST_CATE_CHOICES = [
+		('FG', 'free_giving'),
+		('FW', 'free_want'),
+		('NE', 'new_explorer'),
+		('TS', 'tell_story'),
+	]
+	ANIMAL_CATE_CHOICES = [
+		(),
+		(),
+		(),
+		(),
+	]
+	post_id = models.AutoField(max_length=10, primary_key=True, verbose_name=u"帖子主键ID")
+	title = models.CharField(max_length=20, verbose_name=u"帖子标题")
+	content = models.CharField(max_length=512, null=True, blank=True, verbose_name=u"帖子内容")
+	add_time = models.DateTimeField(default=timezone.now, verbose_name=u"发帖时间")
+	mod_time = models.DateTimeField(auto_now_add=True, verbose_name=u"修订时间")
+	image = models.ImageField(upload_to='post_img', null=True, verbose_name=u"贴图")
+	user_id = models.IntegerField(verbose_name=u"所属用户")
+	post_cate = models.CharField(max_length=2, verbose_name="帖子类型")
+	animal_cate = models.CharField(max_length=10, null=True, verbose_name="动物类型")
+
+	class Meta:
+		verbose_name = u"帖子"
+		verbose_name_plural = verbose_name
+		db_table = "post"
+		ordering = "-mod_time"
--- register/models.py
+++ register/models.py
@@ -2,29 +2,31 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import django.utils.timezone as timezone
 
 # Create your models here.
 
 class UserInfo(models.Model):
-	#主键自增
-	#user_id = models.CharField(max_length=10, primary_key=True, verbose_name=u"主键ID")
+	
 	USER_KIND_CHOICES = [
 		('A', u'Individual'),
 		('B', u'Individual_plus'),
 		('C', u'Station'),
 		('D', u'Station_plus'),
-		('E', u'Admin'),
 	]
+	user_id = models.AutoField(max_length=10, primary_key=True, verbose_name=u"用户主键ID")
 	username = models.CharField(max_length=16, null=True, blank=True, verbose_name=u"用户名")
+	password = models.CharField(max_length=16,verbose_name=u"密码")
 	kind = models.CharField(max_length=1, choices=USER_KIND_CHOICES, verbose_name=u"用户类型")
-	regtime = models.DateTimeField()
-	last_update_time = models.DateField()
+	mod_time = models.DateTimeField(auto_now_add=True, verbose_name="修订时间")
+	add_time = models.DateTimeField(default=timezone.now, verbose_name=u"注册时间")
 	phone = models.CharField(max_length=20, verbose_name=u"联系电话")
 	email = models.EmailField(null=True, blank=True, verbose_name=u"邮箱")
 	adress = models.CharField(max_length=45, null=True, blank=True, verbose_name=u"地址")
-	icon = models.ImageField()
-	creditrating = models.IntegerField()
-	ip = models.IPAdressField()
+	icon = models.ImageField(upload_to='user_icon', null=True, verbose_name=u"头像")
+	photo = models.ImageField(upload_to='user_photo', null=True, verbose_name=u"相册")
+	creditrating = models.IntegerField(default=60, verbose_name=u"信誉评分")
+	#ip = models.IPAdressField(null=True, blank=True, verbose_name=u"IP地址")
 
 	class Meta:
 		verbose_name = u"用户信息"
