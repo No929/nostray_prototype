d85cc8883da230ba8b905f51bb6f40bd010662c0
Klawens <lscllyx@icloud.com>
2017-06-13 20:22:13 +0800

    Register prototype

--- register/views.py
+++ register/views.py
@@ -3,7 +3,23 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import UserInfo
 # Create your views here.
 
 def regist(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username', '')
+		passwaord = request.POST.get('password', '')
+		kind = request.POST.get('kind', '')
+		phone = request.POST.get('phone', '')
+
+		form = UserInfo()
+		form.username = username
+		form.password = password
+		form.kind = kind
+		form.phone = phone
+
+		form.save()
+
 	return render(request, 'regist.html')
