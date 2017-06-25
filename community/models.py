5f827245004d7cba4a1745398f9219f97752dcf3
Klawens <lscllyx@icloud.com>
2017-06-25 21:53:06 +0800

    fix problem

--- community/models.py
+++ community/models.py
@@ -20,11 +20,11 @@ class Posts(models.Model):
 	title = models.CharField(max_length=100, verbose_name=u"帖子标题")
 	content = models.TextField(null=True, blank=True, verbose_name=u"帖子内容")
 	add_time = models.DateTimeField(default=timezone.now, verbose_name=u"发帖时间")
-	image = models.ImageField(maxlength=100, upload_to='post_img', null=True, verbose_name=u"贴图")
+	image = models.ImageField(max_length=100, upload_to='post_img', null=True, verbose_name=u"贴图")
 	user_id = models.IntegerField(verbose_name=u"所属用户")
 	post_cate = models.CharField(max_length=2, choices=POST_CATE_CHOICES, verbose_name="帖子类型")
 	animal_cate = models.CharField(max_length=10, choices=ANIMAL_CATE_CHOICES, null=True, verbose_name="动物类型")
-	comments = models.IntegerField(default=0, verbose_name=u"评论数量")
+	comment_num = models.IntegerField(default=0, verbose_name=u"评论数量")
 	likes = models.IntegerField(default=0, verbose_name=u"顶")
 	dislikes = models.IntegerField(default=0, verbose_name=u"踩")
 
@@ -34,8 +34,8 @@ class Posts(models.Model):
 
 
 class Comments(models.Model):
-	post = models.ForeignKey(Post, verbose_name=u"帖子")
-	content = models.CharField(max_length=256, verbose_name=u"内容")
+	post = models.ForeignKey(Posts, verbose_name=u"帖子")
+	content = models.TextField(verbose_name=u"内容")
 	user_id = models.IntegerField(verbose_name=u"所属用户")
 	image = models.ImageField(max_length=100, upload_to='comment_img', null=True, verbose_name=u"评论图片")
 	add_time = models.DateTimeField(default=timezone.now, verbose_name=u"评论时间")
