f5660d43cf51d8e7833ae1b55b792c868caa614d
Klawens <lscllyx@icloud.com>
2017-07-02 23:17:44 +0800

    Deleted center and added animal app

--- animal/apps.py
+++ animal/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class CenterConfig(AppConfig):
-    name = 'center'
+class AnimalConfig(AppConfig):
+    name = 'animal'
