6725691ca4aeeeab9fe42677e2f540002ca82c65
Klawens <lscllyx@icloud.com>
2017-07-02 23:49:49 +0800

    UserFavorite table

--- operation/models.py
+++ operation/models.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
-
+from register.models import UserInfo
+#from animal.models import Animals
+#from community.models import Posts
 # Create your models here.
+
+
+class UserFavorite(models.Model):
+	FAV_TYPE_CHOICES = [
+		(1, 'animal'),
+		(2, 'post'),
+	]
+	user = models.ForeignKey(UserInfo, verbose_name=u"用户")
+	fav_id = models.IntegerField(default=0, verbose_name=u"数据ID")
+	fav_type = models.IntegerField(choices=FAV_TYPE_CHOICES, default=1, verbose_name=u"收藏类型")
+
+	class Meta:
+		verbose_name = u"用户收藏"
+		verbose_name_plural = verbose_name
+
+'''
+class UserMessage(models.Model):
+
+
+	class Meta:
+		verbose_name = u"用户消息"
+		verbose_name_plural = verbose_name
+'''
