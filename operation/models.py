32326b087de8d4815268c63bfa76f146392653c5
Klawens <lscllyx@icloud.com>
2017-07-03 13:23:37 +0800

    Add UserMessage table

--- operation/models.py
+++ operation/models.py
@@ -22,11 +22,13 @@ class UserFavorite(models.Model):
 		verbose_name = u"用户收藏"
 		verbose_name_plural = verbose_name
 
-'''
-class UserMessage(models.Model):
 
+class UserMessage(models.Model):
+	user = models.IntegerField(default=0, verbose_name=u"接受用户")
+	message = models.CharField(max_length=500, verbose_name=u"内容")
+	has_read = models.BooleanField(default=False, verbose_name=u"是否已读")
+	add_time = models.DateTimeField(default=datetime.now,verbose_name=u"发送时间")
 
 	class Meta:
 		verbose_name = u"用户消息"
 		verbose_name_plural = verbose_name
-'''
