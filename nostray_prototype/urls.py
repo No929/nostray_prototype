da83ee16f49c62d21db78c8c90c13a51ace73c4a
Klawens <lscllyx@icloud.com>
2017-07-06 17:39:49 +0800

    Fixed some urls

--- nostray_prototype/urls.py
+++ nostray_prototype/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from register import views
-from index.views import index as index
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^login/$', TemplateView.as_view(template_name='login.html'), name='login'),
     url(r'^regist/$', views.regist),
-    url(r'^index/$', index),
+
+    
     #url(r'^community/', include('community.urls', namespace='community')),
     #url(r'^raise/', include('raise.urls', namespace='raise')),
 ]
