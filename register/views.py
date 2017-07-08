77dbbeb33acde1e62e6e08da98ef610861ca13db
Klawens <lscllyx@icloud.com>
2017-07-08 11:14:25 +0800

    Finish class replace def

--- register/views.py
+++ register/views.py
@@ -8,12 +8,13 @@ from django.db.models import Q
 from django.views.generic.base import View
 
 from .models import UserInfo
+from .forms import LoginForm
 
 
 class CustomBackend(ModelBackend):
 	def authenticate(self, username=None, password=None, **kwargs):
 		try:
-			user = UserInfo.objects.get(Q(email=username)|Q(phone=username)|Q(username=username))
+			user = UserInfo.objects.get(Q(phone=username)|Q(username=username))
 			if user.check_password(password):
 				return user
 		except Exception as e:
@@ -26,14 +27,16 @@ class LoginView(View):
 	def get(self, request):
 		return render(request, "login.html", {})
 	def post(self, request):
-		username = request.POST.get('username', '')
-		password = request.POST.get('password', '')
-		user = authenticate(username=username, password=password)
-		if user is not None:
-			login(request, user)
-			return render(request, 'index.html')
+		login_form = LoginForm(request.POST)
+		if login_form.is_valid():
+			username = request.POST.get('username', '')
+			password = request.POST.get('password', '')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return render(request, 'index.html')
 		else:
-			return render(request, 'login.html', {'msg':'用户名或密码错误！'})
+			return render(request, 'login.html', {'msg':'用户名或密码错误！'}, {'login_form':login_form})
 
 
 def regist(request):
