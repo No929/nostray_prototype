d29961c61855ce6f916408a34630a8c1d7b537ab
Klawens <lscllyx@icloud.com>
2017-07-09 23:17:23 +0800

    add registe form detection

--- register/forms.py
+++ register/forms.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
+from captcha.fields import CaptchaField
 
 
 class LoginForm(forms.Form):
 	username = forms.CharField(required=True, min_length=2, max_length=16)
 	password = forms.CharField(required=True, min_length=6, max_length=16)
+
+
+class RegisteForm(forms.Form):
+	username = forms.CharField(required=True, min_length=2, max_length=16)
+	password = forms.CharField(required=True, min_length=6, max_length=16)
+	captcha = CaptchaField()
