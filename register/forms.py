b9acf22d350436b0b83a9d26819c5f92bc98ea9f
Klawens <lscllyx@icloud.com>
2017-07-11 16:30:06 +0800

    Alert

--- register/forms.py
+++ register/forms.py
@@ -12,4 +12,4 @@ class LoginForm(forms.Form):
 class RegisteForm(forms.Form):
 	username = forms.CharField(required=True, min_length=2, max_length=16)
 	password = forms.CharField(required=True, min_length=6, max_length=16)
-	captcha = CaptchaField()
+	captcha = CaptchaField(error_messages={"invalid":u"验证码错误"})
