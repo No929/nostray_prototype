f1e69ff0d56688b800166f85eba53b4276c93acd
Klawens <lscllyx@icloud.com>
2017-07-19 21:42:24 +0800

    startapp adopt

---
+++ adopt/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views.generic.base import View
+
+# Create your views here.
+
+
+class ShowNearBy(View):
+	pass
