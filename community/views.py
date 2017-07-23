bd9519a9f288d55d52f657a6edaa6c1870427a37
Klawens <lscllyx@icloud.com>
2017-07-23 16:12:01 +0800

    Plan to remake

--- community/views.py
+++ community/views.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic.base import View
 
 # Create your views here.
 
 
-def allPosts(request):
-	pass
+class AllPosts(View):
+	def get(self, request):
+		return render(request, 'community.html')
