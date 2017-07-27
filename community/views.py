028ebfbaf7338cb850083d769f98e2c5ac881a8d
Klawens <lscllyx@icloud.com>
2017-07-27 22:12:11 +0800

    filter, order_by accomplished
    feeling good

--- community/views.py
+++ community/views.py
@@ -6,6 +6,7 @@ from django.views.generic.base import View
 
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from .models import Posts
+from operation.models import UserFavorite, UserMessage
 from register.models import UserInfo
 # Create your views here.
 
@@ -17,9 +18,18 @@ class AllPosts(View):
 		animal_kind = request.GET.get('animal', '')
 		post_kind = request.GET.get('kind', '')
 		if animal_kind:
-			all_posts = all_posts.filter(animal_cate=animal_kind)
+			all_posts = all_posts.filter(animal_cate=animal_kind).order_by("-likes")
 		if post_kind:
-			all_posts = all_posts.filter(post_cate=post_kind)
+			all_posts = all_posts.filter(post_cate=post_kind).order_by("-likes")
+
+		sort = request.GET.get('sort', '')
+		if sort:
+			if sort == 'add_time':
+				all_posts = all_posts.order_by('-add_time')
+			elif sort == 'comment_num':
+				all_posts = all_posts.order_by('-comment_num')
+			elif sort == 'likes':
+				all_posts = all_posts.order_by('-likes')
 
 		post_num = all_posts.count()
 
@@ -31,9 +41,21 @@ class AllPosts(View):
 		p = Paginator(all_posts, 7, request=request)
 		posts = p.page(page)
 
+		my_fav = UserFavorite.objects.all()
+		my_fav_num = my_fav.filter(user=request.user).count()
+
+		my_posts_num = all_posts.filter(user=request.user).count()
+
+		my_msg = UserMessage.objects.all()
+		my_msg_num = my_msg.filter(user=request.user, has_read=False).count()
+
 		return render(request, 'community.html', {
 			"all_posts" : posts,
 			"post_num" : post_num,
 			"animal_kind" : animal_kind,
 			"post_kind" : post_kind,
+			"sort" : sort,
+			"my_fav_num" : my_fav_num,
+			"my_posts_num" : my_posts_num,
+			"my_msg_num" : my_msg_num,
 			})
