3ab3c55f0b1a4d9e4943096e74e5d0d84d8be846
Klawens <lscllyx@icloud.com>
2017-07-30 22:57:51 +0800

    something wrong with the forms

--- community/forms.py
+++ community/forms.py
@@ -4,7 +4,8 @@ from django import forms
 
 from .models import Posts
 
-class PostForm(forms.Form):
-    title = forms.CharField(required=True, min_length=1, max_length=20)
-    content = forms.CharField(required=True, min_length=1, max_length=2000)
-    image = forms.ImageField(required=True)
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Posts
+        fields = ['image', 'title', 'content']
