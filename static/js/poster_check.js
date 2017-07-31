588f90aac9de05d89ad39d36309672482e485ad8
Klawens <lscllyx@icloud.com>
2017-07-31 16:09:50 +0800

    add a clear img button

--- static/js/poster_check.js
+++ static/js/poster_check.js
@@ -1,5 +1,3 @@
-var flag1, flag2, flag3 = false;
-
 function titleChange(obj) {
     if (obj.length>20 || obj.length<1)
         document.getElementById('title_error').innerHTML =
@@ -11,32 +9,28 @@ function titleChange(obj) {
 }
 
 function contentChange(obj) {
-    if (obj.length>5 || obj.length<1)
+    if (obj.length>2000 || obj.length<1)
         document.getElementById('content_error').innerHTML =
             '<span>内容长度不规范(1-2000)</span>';
     else {
         document.getElementById('content_error').innerHTML =
             '<span></span>';
-        flag2 = true;
     }
 }
 
 function imgChange(obj) {
     if (obj.length != 0) {
-        flag3 = true;
         document.getElementById("img_error").innerHTML =
-            "<span></span>"
+            "<span></span>";
         var path = document.getElementById("img_input").value;
         document.getElementById("img_root").innerHTML = path;
+    if (onj.length == 0) {
+        document.getElementById("img_error").innerHTML = "未上传图片";
+    }
     }
 }
 
-function poster_submit() {
-    if (flag3 == false)
-        document.getElementById("img_error").innerHTML =
-            "<span>未上传图片</span>";
-    var title = document.getElementById("title").value
-    var content = document.getElementById("content").value
-    if (title == '' || content == '')
-        alert('请填检查内容完整性');
+function clearImg() {
+    document.getElementById("img_input").value = '';
+    document.getElementById("img_root").innerHTML = '无';
 }
