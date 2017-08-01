f684ca4f00346900b13dd08a759b005e11d0318d
Klawens <lscllyx@icloud.com>
2017-08-01 18:26:03 +0800

    passing the dtp

--- index/views.py
+++ index/views.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from register.models import UserInfo
+
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	all_user = UserInfo.objects.all()
+	user = all_user.filter(username=request.user)
+	return render(request, 'index.html', {
+		"icon": user.icon,
+		"user": user.username,
+	})
