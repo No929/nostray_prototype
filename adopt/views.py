1c2c95f01e9a78cb306c9a004000a80f25cf3dc8
Klawens <lscllyx@icloud.com>
2017-08-01 18:26:36 +0800

    create adopt base

--- adopt/views.py
+++ adopt/views.py
@@ -4,8 +4,17 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from register.models import UserInfo
+
 # Create your views here.
 
 
 class ShowNearBy(View):
-	pass
+
+	def get(self, request):
+		all_user = UserInfo.objects.all()
+		user = all_user.get(username=request.user)
+		return render(request, "adopt.html", {
+			'icon': user.icon,
+			'user': user.username,
+		})
