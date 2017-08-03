d6022f2602e1b3bd73e0d04d1392264c43e10a35
Klawens <lscllyx@icloud.com>
2017-08-03 22:54:09 +0800

    use pagination

--- adopt/views.py
+++ adopt/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 
 from register.models import UserInfo
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -14,7 +15,18 @@ class ShowNearBy(View):
 	def get(self, request):
 		all_user = UserInfo.objects.all()
 		user = all_user.get(username=request.user)
+
+		all_stations = UserInfo.objects.filter(kind='station')
+		try:
+			page = request.GET.get('page', 1)
+		except PageNotAnInteger:
+			page = 1
+
+		p = Paginator(all_stations, 12, request=request)
+		stations = p.page(page)
+
 		return render(request, "adopt.html", {
 			'icon': user.icon,
 			'user': user.username,
+			'all_stations': stations,
 		})
