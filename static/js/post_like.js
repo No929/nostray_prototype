80926f396b2d642a981841d152db9d0364ae9647
Klawens <lscllyx@icloud.com>
2017-08-08 19:13:15 +0800

    a praise button

---
+++ static/js/post_like.js
@@ -0,0 +1,23 @@
+ $(function(){
+		$("#praise").click(function(){
+			var praise_img = $("#praise-img")
+			var text_box = $("#add-num")
+			var praise_txt = $("#praise-txt")
+			var num=parseInt(praise_txt.text())
+			if(praise_img.attr("src") == ("/static/img/liked.png")){
+				$(this).html("<img src='/static/img/like.png' id='praise-img' class='animation' />")
+				praise_txt.removeClass("hover")
+				text_box.show().html("<em class='add-animation'>-1</em>")
+				$(".add-animation").removeClass("hover")
+				num -=1
+				praise_txt.text(num)
+			}else{
+				$(this).html("<img src='/static/img/liked.png' id='praise-img' class='animation' />")
+				praise_txt.addClass("hover")
+				text_box.show().html("<em class='add-animation'>+1</em>")
+				$(".add-animation").addClass("hover")
+				num +=1
+				praise_txt.text(num)
+			}
+		})
+	})
