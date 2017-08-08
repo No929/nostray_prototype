2ea24efbb48476e9fcbbcae26b25f1de49829f6a
Klawens <lscllyx@icloud.com>
2017-08-08 19:12:34 +0800

    ajax praise url

--- community/urls.py
+++ community/urls.py
@@ -2,10 +2,11 @@
 
 from django.conf.urls import url, include
 
-from community.views import AllPosts, PosterView
+from community.views import AllPosts, PosterView, likeView
 
 
 urlpatterns =[
     url(r'^allpost/$', AllPosts.as_view(), name='allpost'),
     url(r'^poster/$', PosterView.as_view(), name='poster'),
+    url(r'^like/$', likeView, name='like')
 ]
