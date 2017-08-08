2bcbd5b4f455441a31b0855f2c6fadbf290d544f
Klawens <lscllyx@icloud.com>
2017-08-08 19:10:39 +0800

    moved inside jQ out

---
+++ static/js/poster_ajax.js
@@ -0,0 +1,24 @@
+ $(document).ready(function () {
+        $("#submitBtn").click(function () {
+            $.ajax({
+                type:"POST",
+                url:"/community/poster/",
+                dataType:"json",
+                data:{
+                    title:$("#title").val(),
+                    content:$("#content").val(),
+                    image:$("#image").val(),
+                },
+                success:function(data){
+                    if (data.success){
+                        $("#img_error").html(data.msg)
+                    } else {
+                        $("#img_error").html(data.msg)
+                    }
+                },
+                error:function (jqXHR) {
+                    alert("发生错误："+jqXHR.status)
+                }
+            })
+        })
+    })
