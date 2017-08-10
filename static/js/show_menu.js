fa9d8726a83da9b63a92c0fd9cd61f20d195a243
Klawens <lscllyx@icloud.com>
2017-08-10 15:33:58 +0800

    add mode choice (front end)

---
+++ static/js/show_menu.js
@@ -0,0 +1,19 @@
+$(function(){
+	$(".ul_li").children(".ul_li_ul").hide();
+	$(".ul_li").mouseover(function(){
+		 $(this).children(".ul_li_ul").show();
+		 })
+	 $(".ul_li").children(".ul_li_ul").mouseout(function(){
+		 $(this).hide();
+		 })
+     $('#a0').mousedown(function() {
+		 $(".ul_li").children(".ul_li_ul").hide();
+	 })
+	 $('#a1').mousedown(function() {
+        var txt1 = $('.ul_li').children('#a0').html();
+        var txt = $('#li1').children('#a1').html();
+        $('#li1').children('#a1').html(txt1);
+        $('.ul_li').children('#a0').html(txt);
+		$(".ul_li").children(".ul_li_ul").hide();
+    });
+})
