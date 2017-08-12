71c64280cf09873f4c1f0208c595bb67589b8406
Klawens <lscllyx@icloud.com>
2017-08-12 20:02:30 +0800

    drop ModelForm

--- community/forms.py
+++ community/forms.py
@@ -4,8 +4,12 @@ from django import forms
 
 from .models import Posts
 
-class PostForm(forms.ModelForm):
-
+class PostForm(forms.Form):
+    image = forms.ImageField(required=False, max_length=100)
+    title = forms.CharField(required=True, min_length=6, max_length=20)
+    content = forms.CharField(required=True, min_length=2, max_length=1000)
+    '''
     class Meta:
         model = Posts
         fields = ['image', 'title', 'content']
+        '''
