03632f1d14a7a1096eff5cc2d2a7503427f58b42
Klawens <lscllyx@icloud.com>
2017-08-24 15:26:52 +0800

    rename poster ajax

--- static/js/collect.jQuery.js
+++ static/js/collect.jQuery.js
@@ -6,20 +6,17 @@ function add_fav(current_elem, fav_id, fav_type, csrf){
         data:{'fav_id':fav_id, 'fav_type':fav_type},
         async: true,
         beforeSend:function(xhr, settings){
-            xhr.setRequestHeader("X-CSRFToken", csrf);
+            xhr.setRequestHeader("X-CSRFToken", csrf)
         },
         success: function(data) {
             if(data.status == 'fail'){
-                if(data.msg == '未登录'){
-                    window.location.href = '/login/';
-                }else{
+                if(data.msg == 'NO_LOGIN')
+                    window.location.href = '/login/'
+                else
                     current_elem.html(data.msg)
-
-                }
-
             }else if(data.status == 'success'){
                 current_elem.html(data.msg)
             }
         },
-    });
+    })
 }
